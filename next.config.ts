59566d78d4b2786d0b12f087464082f421941f4d
Muhammad Ahmed Shehzad <ahmedshehzad786@gmail.com>
2025-07-04 20:42:12 +0200

    static export

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   turbopack: {
     // Change here from `experimental.turbo` to `turbopack`
     rules: {
