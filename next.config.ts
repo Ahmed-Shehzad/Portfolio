0761a6580e9a5a0be23bef1d15c9e0105350cfc1
Muhammad Ahmed Shehzad <ahmedshehzad786@gmail.com>
2025-07-05 00:02:53 +0200

    tailwindcss applied

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,16 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.DEPLOYED_GITHUB_PATH ?? "",
-  assetPrefix: process.env.DEPLOYED_GITHUB_PATH ?? "",
   images: {
     unoptimized: true,
   },
+  ...(isProd && {
+    basePath: "/Portfolio",
+    assetPrefix: "/Portfolio",
+  }),
   turbopack: {
     rules: {
       "*.svg": {
