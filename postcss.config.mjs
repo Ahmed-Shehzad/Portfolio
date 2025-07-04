47b56132856541aab65754de27ab30ee3451f1a9
Muhammad Ahmed Shehzad <ahmedshehzad786@gmail.com>
2025-07-04 23:14:24 +0200

    fix: use ESM imports in postcss.config.mjs for Next.js/Tailwind

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,11 @@
+
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+
 const config = {
   plugins: [
-    "tailwindcss",
-    "autoprefixer"
+    tailwindcss,
+    autoprefixer
   ],
 };
 
